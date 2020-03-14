829bf51ff2bbce8bda3d72344f2abd42d7f09f7e
kittykssum <kittykssum@gmail.com>
2020-03-14 12:13:01 +0800

    install jquery ui rails

---
+++ app/assets/javascripts/testing.js
@@ -0,0 +1,17 @@
+/* 
+
+with the statement:
+//= require_tree .
+in application.js
+
+all javascript files located in the javascripts folder is called on every page 
+
+<script src="/assets/testing.self-0a1b907325f18721253e6a4c8fd791aa0010b0f2ea4369c95f67d82096a91c1b.js?body=1" data-turbolinks-track="reload"></script>
+
+*/
+
+/* 
+
+alert('hello pinky');
+
+*/
